0d9f531446ec54cad944eab3ca135af38585681a
elie <inahon.elie17@yahoo.com>
2025-01-16 20:46:26 -0500

    Added Repository Layer

---
+++ src/main/java/com/el/EmployeeGradleDataManagement1/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.el.EmployeeGradleDataManagement1;
+
+import com.el.EmployeeGradleDataManagement1.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    public Employee findByNameIgnoreCase(String name);
+}
