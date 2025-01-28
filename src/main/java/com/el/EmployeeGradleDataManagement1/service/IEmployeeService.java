1518d1cb5bb385992b47d72e2cad037e90233978
elie <inahon.elie17@yahoo.com>
2025-01-27 23:32:32 -0500

    Implemented custom exception handling for employee-related operations by creating an error package and defining the EmployeeNotFoundException class. Integrated the exception handling into the endpoints for retrieving an employee by ID, retrieving an employee by name, deleting an employee, and updating an employee. Ensured that appropriate exceptions are thrown when an employee is not found, and method signatures added accordingly.

--- src/main/java/com/el/EmployeeGradleDataManagement1/service/IEmployeeService.java
+++ src/main/java/com/el/EmployeeGradleDataManagement1/service/IEmployeeService.java
@@ -1,5 +1,6 @@
 package com.el.EmployeeGradleDataManagement1.service;
 
+import com.el.EmployeeGradleDataManagement1.error.EmployeeNotFoundException;
 import com.el.EmployeeGradleDataManagement1.model.Employee;
 
 import java.util.List;
@@ -8,8 +9,8 @@ public interface IEmployeeService {
 
 public Employee saveEmployee(Employee employee);
 public List<Employee> fetchEmployeeList();
-public Employee updateEmployeeById(Long id, Employee employee);
-public void deleteEmployeeById(Long id);
-public Employee fetchEmployeeById(Long id);
-public Employee fetchEmployeeByName(String name);
+public Employee updateEmployeeById(Long id, Employee employee) throws EmployeeNotFoundException;
+public void deleteEmployeeById(Long id) throws EmployeeNotFoundException;
+public Employee fetchEmployeeById(Long id) throws EmployeeNotFoundException;
+public Employee fetchEmployeeByName(String name) throws EmployeeNotFoundException;
 }
